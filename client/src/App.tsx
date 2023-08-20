0b5ea2a56d030d449b441675c8b10abe364f4b61
Landon Connell <landonbconnell2001@gmail.com>
2023-08-20 15:26:17 -0400

    added default route to react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { createTheme, CssBaseline } from "@mui/material";
-import IngredientsPage from "components/ingredients/IngredientsPage";
-import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import "./styles/App.css";
-import RecipesPage from "components/recipes/RecipesPage";
-import CakeCostsPage from "components/cakeCosts/CakeCostsPage";
-import Helmet from "react-helmet";
+import React from 'react';
+import { createTheme, CssBaseline } from '@mui/material';
+import IngredientsPage from 'components/ingredients/IngredientsPage';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import './styles/App.css';
+import RecipesPage from 'components/recipes/RecipesPage';
+import CakeCostsPage from 'components/cakeCosts/CakeCostsPage';
+import Helmet from 'react-helmet';
 
 const theme = createTheme({
   breakpoints: {
@@ -21,51 +21,52 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      light: "#ddc9c4",
-      main: "#cbaca2",
-      dark: "#937a74",
-      contrastText: "#303030",
+      light: '#ddc9c4',
+      main: '#cbaca2',
+      dark: '#937a74',
+      contrastText: '#303030',
     },
     secondary: {
-      light: "#ffffff",
-      main: "#f3ebe8",
-      dark: "#f5efed",
+      light: '#ffffff',
+      main: '#f3ebe8',
+      dark: '#f5efed',
     },
   },
   typography: {
     fontFamily: [
-      "Montserrat",
-      "Nunito",
-      "Roboto",
-      "-apple-system",
-      "BlinkMacSystemFont",
+      'Montserrat',
+      'Nunito',
+      'Roboto',
+      '-apple-system',
+      'BlinkMacSystemFont',
       '"Segoe UI"',
-      "Roboto",
+      'Roboto',
       '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
+      'Arial',
+      'sans-serif',
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(","),
+    ].join(','),
   },
 });
 
 const App = () => {
   return (
-    <div className="application">
+    <div className='application'>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta charSet='utf-8' />
         <title>Nicole's Tools</title>
-        <link rel="canonical" href={process.env.REACT_APP_CLIENT_URL} />
+        <link rel='canonical' href={process.env.REACT_APP_CLIENT_URL} />
       </Helmet>
 
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route path="/ingredients" element={<IngredientsPage />} />
-          <Route path="/recipes" element={<RecipesPage />} />
-          <Route path="/cake-costs" element={<CakeCostsPage />} />
+          <Route path='/' element={<IngredientsPage />} />
+          <Route path='/ingredients' element={<IngredientsPage />} />
+          <Route path='/recipes' element={<RecipesPage />} />
+          <Route path='/cake-costs' element={<CakeCostsPage />} />
         </Routes>
       </ThemeProvider>
     </div>
