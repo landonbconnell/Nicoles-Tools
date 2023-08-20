7c3452fc6a90fae229ce93a896116850f23932cf
Landon Connell <landonbconnell2001@gmail.com>
2023-08-20 15:54:31 -0400

    did it seriously fail because I imported useState without using it?

--- client/src/components/recipes/RecipeIngredient.tsx
+++ client/src/components/recipes/RecipeIngredient.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import SelectIngredient from "./SelectIngredient";
-import { Grid, useTheme, useMediaQuery, IconButton } from "@mui/material";
-import SelectUnit from "./SelectUnit";
-import SetQuantity from "./SetQuantity";
-import MobileIngredient from "./MobileIngredient";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { newRecipeSelector } from "redux/selectors/recipeSelectors";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { removeNewRecipeIngredient } from "redux/reducers/recipeSlice";
+import React from 'react';
+import SelectIngredient from './SelectIngredient';
+import { Grid, useTheme, useMediaQuery, IconButton } from '@mui/material';
+import SelectUnit from './SelectUnit';
+import SetQuantity from './SetQuantity';
+import MobileIngredient from './MobileIngredient';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { newRecipeSelector } from 'redux/selectors/recipeSelectors';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { removeNewRecipeIngredient } from 'redux/reducers/recipeSlice';
 
 interface RecipeIngredientProps {
   index: number;
@@ -17,7 +17,7 @@ interface RecipeIngredientProps {
 const RecipeIngredient = ({ index }: RecipeIngredientProps) => {
   const theme = useTheme();
   const dispatch = useDispatch();
-  const isMobile = useMediaQuery(theme.breakpoints.between("xs", "sm"));
+  const isMobile = useMediaQuery(theme.breakpoints.between('xs', 'sm'));
   const { ingredients } = useSelector(newRecipeSelector);
 
   const handleDelete = () => {
@@ -27,16 +27,16 @@ const RecipeIngredient = ({ index }: RecipeIngredientProps) => {
   };
 
   return (
-    <Grid container justifyContent="center" mb="1rem">
+    <Grid container justifyContent='center' mb='1rem'>
       {isMobile ? (
         <MobileIngredient index={index} />
       ) : (
         <Grid
           container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          maxWidth="30rem"
+          direction='row'
+          justifyContent='space-between'
+          alignItems='center'
+          maxWidth='30rem'
           spacing={1}
         >
           <Grid item xs={6}>
@@ -52,10 +52,10 @@ const RecipeIngredient = ({ index }: RecipeIngredientProps) => {
             <IconButton
               disableRipple
               disabled={ingredients.length <= 1}
-              edge="start"
+              edge='start'
               onClick={handleDelete}
             >
-              <DeleteOutlineIcon fontSize="large" />
+              <DeleteOutlineIcon fontSize='large' />
             </IconButton>
           </Grid>
         </Grid>
